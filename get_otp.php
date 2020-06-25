a2b30d61190bbf395c442c926d19ad64cecde842
Neel Patel <neelpatel1905.np@gmail.com>
2020-06-25 20:00:41 +0530

    fix: otp

--- get_otp.php
+++ get_otp.php
@@ -12,7 +12,7 @@
         $_SESSION = $_POST;
         $otp = rand(100000,999999);
         $_SESSION['otp'] = $otp;
-        $_SESSION['otptime'] = new DateTime(date("Y-m-d H:i:s"));;
+        $_SESSION['otptime'] = new DateTime(date("Y-m-d H:i:s"));
         $body = "The One time password for verification is <font color='blue' size='2'><u>$otp</u></font> which is valid for 30 Minutes.";
         sendMail($email,"One Time Password for Verification",$body);
         header("location: verify_email.php");
